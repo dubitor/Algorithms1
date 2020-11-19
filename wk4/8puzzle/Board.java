5fa46e77722a9ef3fc67d9cf62144467799cf19d
dubitor <tom.ashton@blueyonder.co.uk>
2020-11-19 14:57:22 +0000

    added week five files

---
+++ wk4/8puzzle/Board.java
@@ -0,0 +1,317 @@
+import java.util.Stack;
+//import java.util.Scanner;
+import java.util.Arrays;
+//import java.io.File;
+//import java.io.FileNotFoundException; -- see main() comments
+public class Board {
+
+    private final int[][] tiles;
+    private final int n;
+    private int[] blank; // location of blank square -- could this be local to neighbors() method?
+
+    // create a board from an n-by-n array of tiles,
+    // where tiles[row][col] = tile at (row, col)
+    public Board(int[][] tiles) {
+        if (tiles == null) {
+            throw new IllegalArgumentException("parameter 'int[][] tiles' cannot be null");
+        }
+        if (tiles.length < 2 || tiles.length >= 128) {
+            throw new IllegalArgumentException("n must be in range [2, 128)");
+        }
+        this.n = tiles.length;
+        this.tiles = getTilesCopy(tiles);
+    }
+                                           
+    // string representation of this board
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+            s.append(n + "\n");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    s.append(String.format("%2d ", tiles[i][j]));
+                }
+                s.append("\n");
+            }
+        return s.toString();
+    }
+
+    // board dimension n
+    public int dimension() {
+        return n;
+    }
+
+    // number of tiles out of place
+    public int hamming() {
+
+        int ham = 0;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (tiles[row][col] == 0) { ; }// if it's the blank space we don't care
+                else if (!tileInPlace(row, col)) {
+                   ham++;
+                }
+            }
+        }
+        return ham;
+    }
+
+
+    // sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+
+        int man = 0;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (tiles[row][col] != 0) { // we don't want to calculate for the blank tile
+                    int vDist = verticalDistance(row, col); 
+                    int hDist = horizontalDistance(row, col); 
+                    man += vDist + hDist;
+                }
+            }
+        }
+        return man;
+    }
+
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return hamming() == 0;
+    }
+
+    // does this board equal y?
+    // oddly, you can access private instance vars directly
+    // from another instance of the same class
+    public boolean equals(Object y) {
+
+        if (y == null) {
+            return false;
+        }
+        if (this == y) { // do they refer to the same object?
+            return true;
+        }
+        if (this.getClass() != y.getClass()) {
+            return false;
+        }
+
+        Board that = (Board) y; // guaranteed to succeed because of previous condition
+
+        if (this.n != that.n) {
+            return false;
+        }
+        return Arrays.deepEquals(this.tiles, that.tiles);
+    }
+
+
+    // all neighboring boards
+    // a board can have 2, 3, or 4 neighbors, depending 
+    // on location of black square. If blank in corner, 2 neighb.
+    // If blank[x] == 0 or n-1 or blank[y] = 0 or n-1, 3 neigb
+    // Else 4 neighb.
+    // -- find blank square
+    public Iterable<Board> neighbors() {
+        Stack<Board> neighbors = new Stack<Board>();
+        findBlank();
+        if (blank[0] == 0) { // blank's in first column (x = 0)
+            neighbors.push(makeNeighbor("right"));
+            if (blank[1] == 0) { // blank's in top left corner
+                neighbors.push(makeNeighbor("down"));
+                return neighbors;
+            }
+            if (blank[1] == n - 1) { // blank's in bottom left corner
+                neighbors.push(makeNeighbor("up"));
+                return neighbors;
+            }
+            neighbors.push(makeNeighbor("up"));
+            neighbors.push(makeNeighbor("down"));
+            return neighbors;
+        }
+        else if (blank[0] == n - 1) { // blank's in last column
+            neighbors.push(makeNeighbor("left"));
+            if (blank[1] == 0) { // blank's in top right corner
+                neighbors.push(makeNeighbor("down"));
+                return neighbors;
+            }
+            if (blank[1] == n - 1) { // blank's in bottom right corner
+                neighbors.push(makeNeighbor("up"));
+                return neighbors;
+            }
+            neighbors.push(makeNeighbor("up"));
+            neighbors.push(makeNeighbor("down"));
+            return neighbors;
+        }
+        else if (blank[1] == 0) { // blank's somewhere in the middle of first row
+            neighbors.push(makeNeighbor("left"));
+            neighbors.push(makeNeighbor("right"));
+            neighbors.push(makeNeighbor("down"));
+            return neighbors;
+        }
+        else if (blank[1] == n - 1) { // blank's somewhere in the middle of last row
+            neighbors.push(makeNeighbor("left"));
+            neighbors.push(makeNeighbor("right"));
+            neighbors.push(makeNeighbor("up"));
+            return neighbors;
+        }
+        else { // blank's somewhere in the middle
+            neighbors.push(makeNeighbor("left"));
+            neighbors.push(makeNeighbor("right"));
+            neighbors.push(makeNeighbor("up"));
+            neighbors.push(makeNeighbor("down"));
+            return neighbors;
+        }
+    }
+
+
+
+    // a board that is obtained by exchanging any pair of tiles
+    // (the blank is not a tile)
+    public Board twin() {
+        if (tiles[0][0] == 0) {
+            return twin(0, 1, 1, 1);
+        }
+        if (tiles[0][1] == 0) {
+            return twin(0, 0, 1, 0);
+        }
+        return twin(0, 0, 0, 1);
+    }
+
+    // makes a twin by swapping tile (a,b) with (c,d)
+    private Board twin(int a, int b, int c, int d) {
+        int[][] tilesCopy = getTilesCopy();
+        int temp = tilesCopy[a][b];
+        tilesCopy[a][b] = tilesCopy[c][d];
+        tilesCopy[c][d] = temp;
+        return new Board(tilesCopy);
+    }
+
+    // a neighbouring board where the blank has been moved in
+    // the direction supplied. Acceptable inputs are "up", "down",
+    // "left" and "right"
+    private Board makeNeighbor(String direction) {
+        int[][] tilesCopy = getTilesCopy();
+
+        if (direction.equals("up")) {
+            tilesCopy[blank[0]][blank[1]] = tilesCopy[blank[0]][blank[1] - 1];
+            tilesCopy[blank[0]][blank[1] - 1] = 0;
+        }
+        else if (direction.equals("down")) {
+            tilesCopy[blank[0]][blank[1]] = tilesCopy[blank[0]][blank[1] + 1];
+            tilesCopy[blank[0]][blank[1] + 1] = 0;
+        }
+        else if (direction.equals("left")) {
+            tilesCopy[blank[0]][blank[1]] = tilesCopy[blank[0] - 1][blank[1]];
+            tilesCopy[blank[0] - 1][blank[1]] = 0;
+        }
+        else if (direction.equals("right")) {
+            tilesCopy[blank[0]][blank[1]] = tilesCopy[blank[0] + 1][blank[1]];
+            tilesCopy[blank[0] + 1][blank[1]] = 0;
+        }
+        else {
+            throw new IllegalArgumentException("direction must be 'up', 'down', 'left' or 'right'");
+        }
+            
+        return new Board(tilesCopy);
+    }
+
+
+    // the coordinates of the blank square
+    // i.e. [row, col] where tiles[row][col] is blank
+    private void findBlank() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] == 0) {
+                    this.blank = new int[] { i, j };
+                    return;
+                }
+            }
+        }
+        throw new NullPointerException("Couldn't find blank square");
+    }
+
+    // an independent copy of this board's tiles array
+    private int[][] getTilesCopy() {
+        int[][] tilesCopy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            tilesCopy[i] = Arrays.copyOf(tiles[i], n);
+        }
+        return tilesCopy;
+    }
+
+    // an independent copy of the input's tiles array
+    private int[][] getTilesCopy(int[][] tiles) {
+        int[][] tilesCopy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            tilesCopy[i] = Arrays.copyOf(tiles[i], n);
+        }
+        return tilesCopy;
+    }
+
+    // returns true if tiles[x][y] is in the right place
+    // the blank space should NOT be passed to this method
+    // 0-indexed array, so each square should equal x*N+y+1
+    // ---> (0,0) = 1, (1,0) = N+1...
+    // except the last square which should be 0
+    private boolean tileInPlace(int row, int col) {
+        return tiles[row][col] == row * n + col + 1 && !isLastSquare(row, col);
+    }
+
+    // returns true if tiles[x][y] is the last square     
+    private boolean isLastSquare(int row, int col) {
+        return row == n - 1 && col == n - 1;
+    }
+
+    // number of columns away from goal column (i.e. goal col)
+    // the goal column index is tiles[row][col] % N - 1
+    private int horizontalDistance(int row, int col) {
+        int val = tiles[row][col];
+        if (val % n == 0) { // tile should be in last column
+            return (n - 1) - col;
+        }
+        return Math.abs(col - (val % n - 1));
+    }
+
+
+    // number of rows away from goal row (i.e. goal row)
+    // the goal row index of a given value is calculated
+    // by floor dividing that value by n
+    // I've trid to do some sneakiness to overcome the fact
+    // that if the tile's goal column is the last it is difficult
+    // to calculate the goal row (hence the - 1)
+    // n.b. assumes tiles[row][col] != 0
+    private int verticalDistance(int row, int col) {
+        return Math.abs(row - (tiles[row][col] - 1 )/ n);
+    }
+
+    // unit testing (not graded) --- commented out because the grader fails dependencies on certain libraries
+/*    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner sc = new Scanner(new File(args[0]));
+        int n = sc.nextInt();
+        int[][] tiles = new int[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j ++) {
+                tiles[i][j] = sc.nextInt();
+            }
+        }
+
+        Board b = new Board(tiles);
+        System.out.printf("\nn = %d", b.dimension());
+        System.out.printf("\n%s", b.toString());
+        System.out.printf("\nhamming: %d   manhattan: %d", b.hamming(), b.manhattan());
+        System.out.printf("\nis goal? %b", b.isGoal());
+        System.out.printf("\nboard == board? %b\n", b.equals(b));
+
+        Board twin = b.twin();
+        System.out.printf("\ntwin board: \n%s", twin.toString());
+        System.out.printf("\nboard == twin? %b", b.equals(twin));
+        System.out.printf("\ntwin == board? %b\n", twin.equals(b));
+
+        for (Board neighbor : b.neighbors()) {
+            System.out.printf("\nprinting a neighbour: %s", neighbor.toString());
+            System.out.printf("\nhamming: %d   manhattan: %d", neighbor.hamming(), neighbor.manhattan());
+            System.out.printf("\nis goal? %b\n", neighbor.isGoal());
+            System.out.printf("\nneighbour == board? %b\n", neighbor.equals(b));
+        }
+    }*/
+}
---
+++ wk5/kdtree/KdTree.java
@@ -0,0 +1,238 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class KdTree {
+
+    private Node root;
+    private int n;
+
+    // construct an empty set of points
+    public KdTree() {
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    // number of points in the set
+    public int size() {
+        int copy = n;
+        return copy;
+    }
+
+    // add the point to the set (if it's not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) { throw new IllegalArgumentException("Argument cannot be null"); }
+
+        if (!contains(p)) {
+            double[] rectCorners = {0, 0, 1, 1}; // represents rectangle's xmin, ymin, xmax, & ymax
+            root = insertVert(root, p, rectCorners);
+            n++;
+        }
+    }
+    
+    // recursive helper method - is p left or right of node? go right if tie
+    // (odd levels: divides rectangle by drawing vertical line; compare x coords)
+    private Node insertVert(Node node, Point2D p, double[] corners) {
+
+        if (node == null) {
+            return new Node(p, corners);
+        }
+        if (p.x() < node.x()) {
+            corners[2] = node.x(); // change xmax of rectangle if upper x found
+            node.lb = insertHori(node.lb, p, corners);
+        }
+        else {
+            corners[0] = node.x(); // change xmin
+            node.rt = insertHori(node.rt, p, corners);
+        }
+        return node;
+    }
+
+    // recursive helper method - is p above or below node? go up if tie (i.e. right on tree)
+    // (even levels: divides rectangle by drawing horizontal line; compare y coords)
+    private Node insertHori(Node node, Point2D p, double[] corners) {
+
+        if (node == null) {
+            return new Node(p, corners);
+        }
+        if (p.y() < node.y()) {
+            corners[3] = node.y(); // change ymax of rect if node marks top of rect
+            node.lb = insertVert(node.lb, p, corners);
+        }
+        else {
+            corners[1] = node.y(); // change ymin
+            node.rt = insertVert(node.rt, p, corners);
+        }
+        return node;
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null) { throw new IllegalArgumentException("Argument cannot be null"); }
+
+        return containsHori(root, p);
+    }
+
+    // searches the tree for p, using x coords as key
+    // (odd levels)
+    private boolean containsHori(Node node, Point2D p) {
+        if (node == null) {
+            return false;
+        }
+        if (p.equals(node.p)) {
+            return true;
+        }
+        if (p.x() < node.x()) {
+            return containsVert(node.lb, p);
+        }
+        else {
+            return containsVert(node.rt, p);
+        }
+    }
+
+    // searches the tree for p, using y coords as key
+    // (even levels)
+    private boolean containsVert(Node node, Point2D p) {
+        if (node == null) {
+            return false;
+        }
+        if (p.equals(node.p)) {
+            return true;
+        }
+        if (p.y() < node.y()) {
+            return containsVert(node.lb, p);
+        }
+        else {
+            return containsVert(node.rt, p);
+        }
+    }
+
+    // draw all points to standard draw
+    // vertical partitions drawn in red; horizonatal ones in blue
+    public void draw() {
+        drawRed(root);
+    }
+
+    private void drawRed(Node node) {
+        if (node == null) {
+            return;
+        }
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        node.p.draw();
+
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setPenRadius();
+        StdDraw.line(node.x(), node.rect.ymin(), node.x(), node.rect.ymax());
+
+        drawBlue(node.lb);
+        drawBlue(node.rt);
+    }
+        
+    private void drawBlue(Node node) {
+        if (node == null) {
+            return;
+        }
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        node.p.draw();
+
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.setPenRadius();
+        StdDraw.line(node.rect.xmin(), node.y(), node.rect.xmax(), node.y());
+
+        drawBlue(node.lb);
+        drawBlue(node.rt);
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) { throw new IllegalArgumentException("Argument cannot be null"); }
+
+        Queue<Point2D> queue = new Queue<Point2D>();
+        range(root, rect, queue);
+        return queue;
+    }
+
+    private void range(Node node, RectHV rect, Queue<Point2D> queue) {
+        if (node == null) { // return if reach end of tree
+            return;
+        }
+        if (!rect.intersects(node.rect)) { // go back up tree unless rect intersects query rect
+            return;
+        }
+        if (node.isIn(rect)) {
+            queue.enqueue(node.p);
+        }
+        range(node.lb, rect, queue);
+        range(node.rt, rect, queue);
+    }
+
+    // a nearest neighbour in the set to point p; null if the set is empty
+    // n.b. consider equidistant case
+    /*public Point2D nearest(Point2D p) {
+        if (p == null) { throw new IllegalArgumentException("Argument cannot be null"); }
+    }
+    */
+
+    private static class Node {
+        private final Point2D p; // the point
+        private final RectHV rect; // the axis-aligned rectangle corresponding to this node
+        private Node lb; // the left/bottom subtree
+        private Node rt; // the right/top subtree
+
+        public Node(Point2D p, double[] corners) {
+            this.p = p;
+            this.rect = new RectHV(corners[0], corners[1], corners[2], corners[3]);
+        }
+
+        public double x() {
+            return p.x();
+        }
+
+        public double y() {
+            return p.y();
+        }
+
+        public boolean isIn(RectHV rect) {
+            return rect.contains(p);
+        }
+
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
+        Point2D p = new Point2D(0.5, 0.5);
+        Point2D q = new Point2D(0.5, 0.7);
+        Point2D r = new Point2D(0.3, 0.3);
+        
+        KdTree kd = new KdTree();
+        System.out.printf("\nempty tree is empty? %b", kd.isEmpty());
+        kd.insert(p);
+        kd.insert(q);
+        kd.insert(r);
+        System.out.printf("\ntree with 3 pts empty? %b size? %d", kd.isEmpty(), kd.size());
+        System.out.printf("\ncontains those three points? %b %b %b", kd.contains(p), kd.contains(q), kd.contains(r));
+        System.out.printf("\ncontains point not in it? %b\n\n", kd.contains(new Point2D(0.1, 0.1)));
+
+        RectHV unitSq = new RectHV(0, 0, 1, 1);
+        System.out.printf("\n\npoints within the unit square: \n");
+        for (Point2D point : kd.range(unitSq)) {
+            System.out.print(point.toString() + "\n");
+        }
+
+        RectHV botHalf = new RectHV(0, 0, 0.5, 0.5);
+        System.out.printf("\n\npoints within the bottom half: \n");
+        for (Point2D point : kd.range(botHalf)) {
+            System.out.print(point.toString() + "\n");
+        }
+
+        kd.draw();
+
+    }
+
+}
