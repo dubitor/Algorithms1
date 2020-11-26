7890e3514be7c04e3eab39243932d1d5bb18d81c
dubitor <tom.ashton@blueyonder.co.uk>
2020-11-26 15:09:53 +0000

    Added perocolation files

---
+++ wk1/Percolation.java
@@ -0,0 +1,105 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    private final WeightedQuickUnionUF UF;
+    private final int n;
+    private final int nSqd;
+    private final int[] openSites;
+    private int numberOpen;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) { throw new IllegalArgumentException("n must be positive"); }
+        this.n = n;
+        nSqd = n * n;
+        UF = new WeightedQuickUnionUF(nSqd + 2);
+        openSites = new int[nSqd + 2];
+        // open virtual top and bottom sites
+        openSites[0] = 1;
+        openSites[nSqd + 1] = 1;
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (outOfRange(row, col)) {
+            throw new IllegalArgumentException("index out of range");
+        }
+        if (isOpen(row, col)) {
+            return;
+        }
+        openSites[gridTo1D(row, col)] = 1;
+        connectToNeighbours(row, col);
+        numberOpen++;
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (outOfRange(row, col)) {
+            throw new IllegalArgumentException("index out of range");
+        }
+        return openSites[gridTo1D(row, col)] == 1;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (outOfRange(row, col)) {
+            throw new IllegalArgumentException("index out of range");
+        }
+        return connected(0, gridTo1D(row, col));
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        int x = numberOpen;
+        return x;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return connected(0, nSqd + 1);
+    }
+
+    // converts row-col index to 1D coord from 1 to n * n
+    // for use in UF data type
+    private int gridTo1D(int row, int col) {
+        return (row - 1) * n + col;
+    }
+
+    // returns false iff no such site in grid
+    private boolean outOfRange(int row, int col) {
+        return row < 1 || row > n || col < 1 || col > n;
+    }
+
+    // is p connected to q? (1D)
+    private boolean connected(int p, int q) {
+        return UF.find(p) == UF.find(q);
+    }
+
+    // connect site at (row, col) to whichever of its neighbours are open and in range
+    private void connectToNeighbours(int row, int col) {
+        connect(row, col, row, col + 1);
+        connect(row, col, row, col - 1);
+        connect(row, col, row + 1, col);
+        connect(row, col, row - 1, col);
+    }
+
+    // connects (r1, c1) to (r2, c2) iff the latter is open and in range
+    // or it is can be interpreted as referring to a virtual site
+    private void connect(int r1, int c1, int r2, int c2) {
+        if (r2 == 0) { // top row to virtual top
+            UF.union(0, gridTo1D(r1, c1));
+            return;
+        }
+        if (r2 == n + 1) { // bottom row to virtual bottom
+            UF.union(gridTo1D(r1, c1), nSqd + 1);
+            return;
+        }
+        if (outOfRange(r2, c2) || !isOpen(r2, c2)) { return; }
+        int p = gridTo1D(r1, c1);
+        int q = gridTo1D(r2, c2);
+        UF.union(p, q);
+    }
+
+    // test client (optional)
+}
---
+++ wk1/PercolationStats.java
@@ -0,0 +1,71 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdOut;
+
+public class PercolationStats {
+    private final double mean;
+    private final double stddev;
+    private final double confidenceLo;
+    private final double confidenceHi;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        double[] thresholds = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            thresholds[i] = thresholdEstimate(n);
+        }
+
+        mean = StdStats.mean(thresholds);
+        stddev = StdStats.stddev(thresholds);
+        confidenceLo = mean - (1.96 * stddev) / (Math.sqrt(trials));
+        confidenceHi = mean + (1.96 * stddev) / (Math.sqrt(trials));
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return mean;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return stddev;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return confidenceLo;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return confidenceHi;
+    }
+
+    // simulates one trial on n-by-n grid 
+    // returns percolation threshold result
+    private double thresholdEstimate(int n) {
+        Percolation perky = new Percolation(n);
+        while (!perky.percolates()) {
+            int row = StdRandom.uniform(1, n + 1);
+            int col = StdRandom.uniform(1, n + 1);
+            perky.open(row, col);
+        }
+        return (double) perky.numberOfOpenSites() / (double) (n * n);
+    }
+    
+    // test client
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int t = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(n, t);
+        StdOut.printf("\nmean\t\t= %.6f", stats.mean());
+        StdOut.printf("\nstddev\t\t= %.6f", stats.stddev());
+        StdOut.printf("\n95%% confidence interval = [%.6f, %.6f]\n", stats.confidenceLo(), stats.confidenceHi());
+    }
+}
